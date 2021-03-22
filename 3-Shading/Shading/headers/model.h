9da4cd3efb8dae0eec48bc4eeb6e9e7052f1a2bf
Amarnath <amarnath2105@gmail.com>
2021-03-22 06:37:16 +0530

    Created model.h

---
+++ 3-Shading/Shading/headers/model.h
@@ -0,0 +1,78 @@
+#ifndef MODEL_H
+#define MODEL_H
+
+#include<iostream>
+#include<glad/glad.h>
+#include<GLFW/glfw3.h>
+#include<glm/glm.hpp>
+#include<glm/gtc/matrix_transform.hpp>
+#include <string>
+#include "headers/cutils.h"
+
+class Model
+{
+public:
+	unsigned int vao, vbo, ebo, program;
+
+	Model(std::string vertShdrName, std::string fragShdrName);
+	
+	void compileShaders(std::string vertShdrName, std::string fragShdrName);
+
+
+};
+
+Model::Model(std::string vertShdrName, std::string fragShdrName)
+{
+	compileShaders(vertShdrName, fragShdrName);
+}
+
+void Model::compileShaders(std::string vertShdrName, std::string fragShdrName)
+{
+	GLuint vertShader, fragShader;
+	//Read Vert Shader file and compile
+	std::string path = "src/";
+	std::string vertStr = GetStringFromFile((path + vertShdrName).c_str());
+	const char* vert = vertStr.c_str();
+	vertShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertShader, 1, &vert, NULL);
+	glCompileShader(vertShader);
+	int success;
+	glGetShaderiv(vertShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		std::cout << "Vert compilation failed";
+		return;
+	}
+
+	//Read Frag Shader file and compile
+	std::string fragStr = GetStringFromFile((path + fragShdrName).c_str());
+	const char* frag = fragStr.c_str();
+	fragShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragShader, 1, &frag, NULL);
+	glCompileShader(fragShader);
+	glGetShaderiv(fragShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		std::cout << "Frag compilation failed";
+		return;
+	}
+
+	//Create program and link 
+	program = glCreateProgram();
+	glAttachShader(program, vertShader);
+	glAttachShader(program, fragShader);
+	glLinkProgram(program);
+	glGetProgramiv(program, GL_LINK_STATUS, &success);
+	glGetProgramiv(program, GL_LINK_STATUS, &success);
+	char infoLog[512];
+	if (!success) {
+		glGetProgramInfoLog(program, 512, NULL, infoLog);
+		std::cout << "Linking Failed:" << infoLog << std::endl;
+	}
+	std::cout << "Shader Compilation Complete \n";
+	glDeleteShader(vertShader);
+	glDeleteShader(fragShader);
+}
+
+#endif // !MODEL_H
+
